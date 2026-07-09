c44309a44b5fe72d2055d69222f626fb57af56d8
Pratik Mahale <pratikmahale2003@gmail.com>
2026-07-09 14:31:59 +0530

    Add files via upload

---
+++ Assignment23/Q1.c
@@ -0,0 +1,89 @@
+///////////////////////////////////////////////////////////////////
+//
+//      File Name : Q1.c
+//      Project   : Logic Building Pattern Assignment
+//
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+//
+//      Including Header File
+//
+///////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////
+//
+//      Function Name : Pattern
+//      Input         : Integer, Integer
+//      Output        : Void
+//      Description   : Display '*' pattern according
+//                      to given rows and columns
+//      Date          : 08/06/2026
+//      Author        : Pratik Subhash Mahale
+//
+///////////////////////////////////////////////////////////////////
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;      // Row counter
+    int j = 0;      // Column counter
+
+    // Traverse each row
+    for(i = 1; i <= iRow; i++)
+    {
+        // Traverse each column
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("* ");
+        }
+
+        // Move cursor to next line
+        printf("\n");
+    }
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//      Main Function
+//
+///////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0;      // Store number of rows
+    int iValue2 = 0;      // Store number of columns
+
+    // Accept rows and columns from user
+    printf("Enter number of rows and columns : ");
+    scanf("%d %d", &iValue1, &iValue2);
+
+    // Function call
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////
+//
+//      Input  :
+//      Rows = 4
+//      Columns = 3
+//
+//      Output :
+//      * * *
+//      * * *
+//      * * *
+//      * * *
+//
+///////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////
+//
+//      Time Complexity : O(iRow * iCol)
+//
+//      Space Complexity : O(1)
+//
+///////////////////////////////////////////////////////////////////
+
